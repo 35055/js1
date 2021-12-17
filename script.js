127a68aaf19c4d650ac34b24eba055cfda914990
Murat <bxndbdbvsj@gmail.com>
2021-12-17 06:13:02 +0300

    murat

---
+++ script.js
@@ -0,0 +1,15 @@
+var str = prompt('Введите ваше имя');
+console.log('Меня зовут ' + str);
+var strAge = +prompt('Сколько вам лет?')
+console.log('Мне ' + strAge);
+var str_plus = prompt('10+5=' )
+console.log('Пример1: 10+5=15 ' + 'Ваш ответ ' + str_plus);
+var str_minus = prompt('20-15=' )
+console.log('Пример2: 20-15=5 ' + 'Ваш ответ ' + str_minus);
+var str_multiply = prompt('7*9=' )
+console.log('Пример1: 7*9=63 ' + 'Ваш ответ ' + str_multiply);
+var str_share = prompt('72/9=' )
+console.log('Пример1: 72/9=8 ' + 'Ваш ответ ' + str_share);
+var str_remains = prompt('10%3=' )
+console.log('Пример1: 10%3=1 ' + 'Ваш ответ ' + str_remains);
+alert('Передийте в консоль')
